918c2313363a4203ff5ed0405dde714a87e52f6e
Muhammad Haider Sheikh <Haidersheikh243@gmail.com>
2024-09-03 16:10:14 +0500

    pushing

---
+++ temp/src/app/admin-dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/components/admin-dashboard/admin-dashboard'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        <Dashboard/>
+    </div>
+  )
+}
