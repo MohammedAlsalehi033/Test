12181151f3b0286eaf5c327bbbf02b0a6455fb1a
MohammedAlsalehi033 <sp22-bse-053@isbstudnet.comsats.edu.pk>
2024-09-03 15:39:59 +0500

    added the actions

---
+++ temp/src/actions/addEventTosociety.ts
@@ -0,0 +1,36 @@
+import { db } from "@/lib/firebaseConfig";
+import { getAuth } from "firebase/auth";
+import { collection, query, where, getDocs, updateDoc, arrayUnion } from "firebase/firestore";
+
+const auth = getAuth();
+
+export const addEventToSociety = async (
+  userEmail: string,
+  newEvent: string
+) => {
+  const user = auth.currentUser;
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  try {
+    const societyCollectionRef = collection(db, "society");
+
+    const q = query(societyCollectionRef, where("email", "==", userEmail));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      querySnapshot.forEach(async (societyDoc) => {
+        await updateDoc(societyDoc.ref, {
+          events: arrayUnion(newEvent)
+        });
+      });
+      
+      console.log("Event added to the society successfully");
+    } else {
+      console.log("No society document found for this email!");
+    }
+  } catch (e) {
+    console.error("Error updating society document: ", e);
+  }
+};
